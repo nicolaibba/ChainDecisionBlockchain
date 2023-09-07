2c88b9de1ea316e464b949fbfd65c8bf3ec7a0aa
nicolaibba <68285957+nicolaibba@users.noreply.github.com>
2023-09-07 16:49:16 -0400

    added form and new views

--- PyChain/blockchain/views.py
+++ PyChain/blockchain/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import datetime
 import hashlib
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from django.views.generic.base import TemplateView
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,13 +16,16 @@ from rest_framework import status
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(nonce = 1, previous_hash = '0')
+        self.create_block(nonce = 1, previous_hash = '0', message=None)
 
-    def create_block(self, nonce, previous_hash):
-        block = {'index': len(self.chain) + 1,
+    def create_block(self, nonce, previous_hash, message):
+        block = {
+                'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'nonce': nonce,
-                 'previous_hash': previous_hash}
+                 'previous_hash': previous_hash,
+                 'message': message
+                 }
         self.chain.append(block)
         return block
 
@@ -64,7 +70,7 @@ class MineBlock(APIView):
     def get(self, request):
         # Get the latest block from your blockchain
         latest_block = blockchain.get_previous_block()
-
+        
         # Your mining logic here (if any)
 
         # Construct a response dictionary
@@ -77,6 +83,29 @@ class MineBlock(APIView):
         }
         # Return the response using DRF's Response class
         return Response(response_data, status=status.HTTP_200_OK)
+
+class MainView(View):
+    
+    template_name = 'PyChain/index.html'
+
+    def get(self, request, *args, **kwargs):
+        latest_block = blockchain.get_previous_block()
+        print(latest_block)
+        print(len(blockchain.chain))
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        
+        message = request.POST.get('message', None)
+        if message:
+            latest_block = blockchain.get_previous_block()
+            nonce = latest_block['nonce']
+            new_nonce = blockchain.proof_of_work(nonce)
+            block_hash = blockchain.hash(latest_block)
+            new_block = blockchain.create_block(new_nonce, block_hash, message)
+            
+        # return render(request, self.template_name)
+        return redirect('homepage')
 
 
 class GetChain(APIView):
